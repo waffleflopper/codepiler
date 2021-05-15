42b94ffde2bc011e3152937d39d5e44a1b93bfcf
Robert Baddeley <waffle@pop-os.localdomain>
2021-05-16 00:21:14 +0900

    css bundling

--- src/plugins/fetch-plugin.ts
+++ src/plugins/fetch-plugin.ts
@@ -31,9 +31,25 @@ export const fetchPlugin = (inputCode: string) => {
         //fetching from unpkg.com
         const { data, request } = await axios.get(args.path);
 
+        // do a .match .css$ to determine if we need to use css loader
+        const filetype = args.path.match(/.css$/) ? "css" : "jsx";
+
+        const escaped = data
+          .replace(/\n/g, "")
+          .replace(/"/g, '\\"')
+          .replace(/'/g, "\\'");
+        const contents =
+          filetype === "css" //can't directly import a css file, so we'll grab the css and plop it in some js
+            ? `
+          const style = document.createElement('style');
+          style.innerText = '${escaped}';
+          document.head.appendChild(style);
+        `
+            : data;
+
         const result: esbuild.OnLoadResult = {
-          loader: "jsx",
-          contents: data,
+          loader: "jsx", //can't use css here or esbuild gets mad since we don't write to files
+          contents,
           resolveDir: new URL("./", request.responseURL).pathname, //ensures packages nested in directories are properly resolved
         };
 
