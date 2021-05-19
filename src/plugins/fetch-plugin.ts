591ab7ef0735346629e5f0c528ae6f101c49d7e4
Robert Baddeley <waffle@pop-os.localdomain>
2021-05-20 03:07:44 +0900

    filter cleanup for css/jsx files

--- src/plugins/fetch-plugin.ts
+++ src/plugins/fetch-plugin.ts
@@ -10,16 +10,17 @@ export const fetchPlugin = (inputCode: string) => {
   return {
     name: "fetch-plugin",
     setup(build: esbuild.PluginBuild) {
-      //onload
-      build.onLoad({ filter: /.*/ }, async (args: any) => {
-        if (args.path === "index.js") {
-          return {
-            loader: "jsx",
-            contents: inputCode,
-          };
-        }
+      build.onLoad({ filter: /(^index\.js$)/ }, () => {
+        return {
+          loader: "jsx",
+          contents: inputCode,
+        };
+      });
 
-        //have we fetched this file?
+      build.onLoad({ filter: /.*/ }, async (args: any) => {
+        // will run for every file, but since it doesn't return anything
+        // esbuilt will go onto other onLoads that match the filter
+        // makes it good for checking cache
         const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(
           args.path
         );
@@ -28,28 +29,43 @@ export const fetchPlugin = (inputCode: string) => {
           return cachedResult;
         }
 
+        return null;
+      });
+
+      build.onLoad({ filter: /.css$/ }, async (args: any) => {
         //fetching from unpkg.com
         const { data, request } = await axios.get(args.path);
-
-        // do a .match .css$ to determine if we need to use css loader
-        const filetype = args.path.match(/.css$/) ? "css" : "jsx";
 
         const escaped = data
           .replace(/\n/g, "")
           .replace(/"/g, '\\"')
           .replace(/'/g, "\\'");
-        const contents =
-          filetype === "css" //can't directly import a css file, so we'll grab the css and plop it in some js
-            ? `
+        const contents = `
           const style = document.createElement('style');
           style.innerText = '${escaped}';
           document.head.appendChild(style);
-        `
-            : data;
+        `;
 
         const result: esbuild.OnLoadResult = {
           loader: "jsx", //can't use css here or esbuild gets mad since we don't write to files
           contents,
+          resolveDir: new URL("./", request.responseURL).pathname, //ensures packages nested in directories are properly resolved
+        };
+
+        //cache it
+        await fileCache.setItem(args.path, result);
+
+        return result;
+      });
+
+      //onload
+      build.onLoad({ filter: /.*/ }, async (args: any) => {
+        //fetching from unpkg.com
+        const { data, request } = await axios.get(args.path);
+
+        const result: esbuild.OnLoadResult = {
+          loader: "jsx", //can't use css here or esbuild gets mad since we don't write to files
+          contents: data,
           resolveDir: new URL("./", request.responseURL).pathname, //ensures packages nested in directories are properly resolved
         };
 
